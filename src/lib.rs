9cd09f115a49354909bdd10f0ce2a1c02e357124
xuan <zx786@vip.qq.com>
2025-07-26 00:17:22 +0800

    feat: 使用html表单

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,34 @@
 use actix_web::dev::Server;
 use actix_web::{App, HttpResponse, HttpServer, web};
 use std::net::TcpListener;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    println!("email: {}, name: {}", form.email, form.name);
+    HttpResponse::Ok().finish()
+}
+
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     // .await
 
     Ok(server)
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // 0 表示让 操作系统自动分配一个空闲的端口
-    // 这是 一种请求“任意可用端口”的写法
-    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
     let port = listener.local_addr()?.port();
     println!("Listening on {}", port);
 
